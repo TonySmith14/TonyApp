8ca94148cbe11a2626d63607962e875bc7a43692
TonySmith14 <112539754+TonySmith14@users.noreply.github.com>
2022-08-31 14:41:22 +0430

    Some Changed in Login File

--- Login.py
+++ Login.py
@@ -1,6 +1,16 @@
 #this is login page for our app
 
-# ----------------------------<<< set User And Pass >>>-----------------------------
+
+# ----------------------------<<< import External File >>>--------------------------
+
+
+FoodList = "Pizza" , "Salad"
+DrinkList = "Cola" , "Sprite"
+
+
+
+# ----------------------------<<< user Information >>>------------------------------
+
 
 U_Name = "Tony Smith"
 UserName = "Tony"
@@ -8,7 +18,8 @@ UserEmail = "tony@smith.com"
 PassWord = "T0NY"
 
 
-# ----------------------------<<< Check User And Pass >>>----------------------------
+
+# ----------------------------<<< User Login >>>------------------------------------
 
 print ("Enter Your User Name or Email Address")
 
@@ -18,10 +29,36 @@ if UserInput == UserName or UserEmail :
     print ("Enter Your Password")
     PassInput = input()
     if PassInput == PassWord :
+        print ("--------------------------------")
         print ("You Logedin")
         print (f"Welcome" , U_Name )
+
     else :
         print ("Your Password is not Correct")        
 else :
     print ("Your User Name or Email Address is not Correct")
 
+print ("--------------------------------")
+
+
+# ----------------------------<<< Select Food >>>-----------------------------------
+
+
+print ("Type Your Food Name : ")
+print (FoodList)
+
+InputFood = input()
+
+if InputFood == FoodList :
+    print("you select this" + InputFood)
+
+
+print ("---------------------------------------------------------")
+
+print ("Now Type Your Drink Name : ")
+print (DrinkList)
+
+InputDrink = input()
+
+if InputDrink == DrinkList :
+    print("you select this" + InputDrink)
